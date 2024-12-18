2463cc59b639087b847640acf416dee82af4462b
PROF. IYANU-OLOGY <iyanuology@gmail.com>
2024-12-18 02:28:52 +0100

    add bulk

--- Bulk_file_renamer.py
+++ Bulk_file_renamer.py
@@ -1,6 +1,7 @@
 import os
 
 path = input("Enter path: ")
+path = path.replace("\\", "/")
 
 print(path)
 
@@ -9,7 +10,7 @@ print(os.listdir(path)) #After writing/pasting your path DON'T FORGET to put '/'
 def main():
     i=1
     for filename in os.listdir(path):
-        new_name = path + str(i) + '.jpg' #write the suitable file extension at the end as per your needs.
+        new_name = path +  str(i) + 'old_picture' + '.jpg' #write the suitable file extension at the end as per your needs.
         old_name = path + filename
         os.rename(old_name,new_name)
         i+=1
